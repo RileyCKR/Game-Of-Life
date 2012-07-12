7fe0ab70858f2657a69356b400df3ee097ed2fd8
Chris Riley <RileyCKR@gmail.com>
2012-07-12 17:58:08 -0400

    Added documentation

--- ConwayGOL/GameRules.cs
+++ ConwayGOL/GameRules.cs
@@ -23,12 +23,22 @@ namespace ConwayGOL
         public int[] BirthRules { get; private set; }
         public int[] StableRules { get; private set; }
 
+        /// <summary>
+        /// Creates a new GameRules object with the given rules.
+        /// </summary>
+        /// <param name="birthRules">A cell will be born if it has this many living neighbors.</param>
+        /// <param name="stableRules">A living cell will survice if it has this many living neighbors.</param>
         public GameRules(int[] birthRules, int[] stableRules)
         {
             this.BirthRules = birthRules;
             this.StableRules = stableRules;
         }
 
+        /// <summary>
+        /// Runs the game rules to determine wether a cell will still be alive.
+        /// </summary>
+        /// <param name="cell">Cell to run rules on.</param>
+        /// <param name="livingNeighbors">The cell's living neighbors.</param>
         public void RunRules(ICell cell, IEnumerable<ICell> livingNeighbors)
         {
             if (cell.IsAlive)
