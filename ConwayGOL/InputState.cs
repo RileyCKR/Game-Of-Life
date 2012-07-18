932773798668dd7b2ae6e28e2cadac9f998fb579
Chris Riley <RileyCKR@gmail.com>
2012-07-18 16:37:43 -0400

    Change default namespace to GameOfLife

--- ConwayGOL/InputState.cs
+++ ConwayGOL/InputState.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace ConwayGOL
+namespace GameOfLife
 {
     /// <summary>
     /// Manages state for all input devices.
--- ConwayGOL/Program.cs
+++ ConwayGOL/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ConwayGOL
+namespace GameOfLife
 {
 #if WINDOWS || XBOX
     static class Program
